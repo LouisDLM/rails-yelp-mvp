c44eec9b69c4dc27f3030775bf3adab91cb60fdc
Louis de La Marnierre <empsalle@gmail.com>
2017-07-05 15:05:45 -0400

    omw to designing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+
+  resources :restaurants do
+    # nested resources for :new, :create => restaurant id is needed to create a review
+    resources :reviews, only: [ :new, :create ]
+
+    # collection do # ===> restaurants/....
+    #   get 'content', to: 'restaurants#content' # GET /restaurants/top
+    # end
+    # member do # =====> restaurants/:id/....
+    #   get 'rating', to: 'restaurants#rating' # GET /restaurants/:id/chef
+    # end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:  "french",
+    phone_number:        "3"
+  },
+  {
+    name:         "La truffière",
+    address:      "chinese",
+    category:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
+    phone_number:        "1"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:  "italian",
+    phone_number:        "3"
+  }
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:  "french",
+    phone_number:        "3"
+  }
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:  "italia",
+    phone_number:        "3"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
